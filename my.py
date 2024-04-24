7b1beb8d1d9edaa5cd519b89e9edccd4625a9926
AntonPekh <163131060+AntonPekh@users.noreply.github.com>
2024-04-24 14:38:00 +0200

    goit-algo-hw-03

--- my.py
+++ my.py
@@ -1,3 +1,5 @@
+# Завдання 1
+
 from datetime import datetime
 
 curency_date= '2020-10-09'
@@ -10,6 +12,23 @@ today_date=datetime.now().date()
 diff_day=((get_days_from_today-today_date).days)
 
 print(diff_day)
+
+###
+
+#Завдання 2
+
+from random import randint
+
+min=1
+max=49
+quality=6
+
+get_numbers_ticke= set()
+
+while len(get_numbers_ticke) !=6:
+    get_numbers_ticke.add(randint(min,max))
+
+print(sorted(list(get_numbers_ticke)))
 
 
 
