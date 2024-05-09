9365536bb7a6e251a970975a6eac1931ee8631db
AntonPekh <163131060+AntonPekh@users.noreply.github.com>
2024-05-09 13:47:30 +0200

    1

---
+++ task_1.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+curency_date= '2020-10-09'
+
+get_days_from_today=datetime.strptime(curency_date, '%Y-%m-%d').date()
+
+today_date=datetime.now().date()
+
+
+diff_day=((get_days_from_today-today_date).days)
+
+print(diff_day)
